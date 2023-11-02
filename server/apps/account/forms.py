3431e0f1cab3ac6c3bdc4515599cf31353ba8029
Anirudh Khurana <anirudh7878@gmail.com>
2023-11-03 01:24:27 +0530

    added login logout

--- server/apps/account/forms.py
+++ server/apps/account/forms.py
@@ -1,21 +1,45 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
 from server.apps.account.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
+    """Custom user registration form inherited from user creation form"""
+
     class Meta(UserCreationForm.Meta):
+        """Added fields to use only 3 fields and not username"""
+
         model = User
         fields = ["email", "password1", "password2"]
 
     def __init__(self, *args, **kwargs):
+        """To add form classes"""
         super().__init__(*args, **kwargs)
         self.fields["email"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "EMAIL ADDRESS"}
+            {"class": "form-control", "placeholder": "EMAIL ADDRESS"},
         )
         self.fields["password1"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "PASSWORD"}
+            {"class": "form-control", "placeholder": "PASSWORD"},
         )
         self.fields["password2"].widget.attrs.update(
-            {"class": "form-control", "placeholder": "CONFIRM PASSWORD"}
+            {"class": "form-control", "placeholder": "CONFIRM PASSWORD"},
         )
+
+
+class UserLoginForm(AuthenticationForm):
+    """User login form inherited from Authentication Form"""
+
+    username = forms.EmailField(
+        required=True,
+        max_length=150,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "EMAIL ADDRESS"},
+        ),
+    )
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "PASSWORD"},
+        ),
+    )
--- server/apps/account/urls.py
+++ server/apps/account/urls.py
@@ -5,5 +5,7 @@ from server.apps.account import views
 app_name = "account"
 
 urlpatterns = [
-    path("register", views.RegistrationView.as_view(), name="register"),
+    path("register", views.UserRegistrationView.as_view(), name="register"),
+    path("login", views.UserLoginView.as_view(), name="login"),
+    path("logout", views.LogoutView.as_view(), name="logout"),
 ]
